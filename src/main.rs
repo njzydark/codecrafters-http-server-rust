a28f53335c57d82b27d66b4be9818e85cc536bb9
njzydark <njzydark@gmail.com>
2024-07-13 14:53:22 +0000

    support parse request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 use std::{
-    fs, io::Write, net::{TcpListener, TcpStream}, thread
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    thread,
 };
+
+struct Request {
+    method: String,
+    path: String,
+    version: String,
+    headers: Vec<String>,
+}
+
+impl Request {
+    fn new(reader: &mut BufReader<TcpStream>) -> Self {
+        let mut request_line = String::new();
+        let mut headers: Vec<String> = Vec::new();
+
+        if let Ok(_) = reader.read_line(&mut request_line) {
+            let mut header_line = String::new();
+            while let Ok(_) = reader.read_line(&mut header_line) {
+                if header_line == "\r\n" {
+                    break;
+                }
+                headers.push(header_line.trim().to_string());
+                header_line.clear();
+            }
+        }
+
+        let request_line_data: Vec<&str> = request_line.split(' ').collect();
+
+        Request {
+            method: request_line_data[0].to_string(),
+            path: request_line_data[1].to_string(),
+            version: request_line_data[2].to_string(),
+            headers,
+        }
+    }
+}
 
 struct Response {
     status: String,
@@ -27,10 +64,20 @@ impl Response {
 fn handle_client(mut stream: TcpStream) {
     println!("accepted new connection");
 
-    let body = fs::read_to_string("hello.html").unwrap();
-    let response = Response::new("200 OK", &body);
+    let mut reader = BufReader::new(stream);
 
-    if let Err(e) = stream.write(response.to_string().as_bytes()) {
+    let request = Request::new(&mut reader);
+
+    // let body = fs::read_to_string("hello.html").unwrap();
+    let response;
+    if request.path.eq("/") {
+        response = Response::new("200 OK", "");
+    } else {
+        response = Response::new("404 Not Found", "");
+    }
+
+    let mut inner = reader.into_inner();
+    if let Err(e) = inner.write(response.to_string().as_bytes()) {
         eprintln!("Failed to write to stream: {}", e);
     }
 }
